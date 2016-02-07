895ffd760e5984c7c0524c82d0ffd116fc43fe94
isvetlic <ivan2svetlicic@gmail.com>
2016-02-06 22:16:08 -0500

    System B files

---
+++ SystemB/DataFrame.java
@@ -0,0 +1,43 @@
+import java.util.Calendar;
+
+
+public class DataFrame {
+
+	//define data frame variables. These will hold the information from each frame
+	Long TimeStamp;
+	Double velocity;
+	Double altitude;
+	Double pressure;
+	Double temperature;
+	Double attitude; 
+	Boolean wildPoint;//stores whether a frame has PSI wild point or not
+	Double wildPSI;
+	
+	//constructor
+	public DataFrame(Long TS, Double vel, Double alt, 
+			Double press, Double temp, Double att, Boolean WP, Double wPSI) 
+	{
+		TimeStamp = TS;
+		velocity = vel;
+		altitude = alt;
+		pressure = press;
+		temperature = temp;
+		attitude = att;
+		wildPoint = WP;
+		wildPSI = wPSI;
+	}
+	
+	//default constructor
+	public DataFrame ()
+	{
+		TimeStamp = null;
+		velocity = null;
+		altitude = null;
+		pressure = null;
+		temperature = null;
+		attitude = null;
+		wildPoint = null;
+		wildPSI = null;
+	}
+	
+}
